ef0560e4576e711da7963656e79a8bfaf7e989f1
Mal Graty <mal.graty@googlemail.com>
2014-09-14 15:42:20 +0100

    Microised

--- src/rsautl.cc
+++ src/rsautl.cc
@@ -12,158 +12,56 @@
     return NanThrowTypeError("Argument " #I " must be a buffer");             \
   Local<Object> _ ## VAR = args[I]->ToObject();                               \
   char *VAR = Buffer::Data(_ ## VAR);                                         \
-  size_t VAR ## _len = Buffer::Length(_ ## VAR);
+  size_t VAR ## _len = Buffer::Length(_ ## VAR); 
+
+#define KEY_ARGS_private key_pem, key_pem_len, NULL
+#define KEY_ARGS_public  key_pem, key_pem_len
+
+#define RSAUTL_METHOD(NAME, KEY, OP)                                          \
+NAN_METHOD(NAME) {                                                            \
+  NanScope();                                                                 \
+  LOG("Beginning " #NAME)                                                     \
+                                                                              \
+  int out_len = 0;                                                            \
+  char *out = NULL;                                                           \
+  int pad = RSA_PKCS1_PADDING;                                                \
+  bool fatal = true;                                                          \
+                                                                              \
+  REQ_BUF_ARG(0, in)                                                          \
+  REQ_BUF_ARG(1, key_pem)                                                     \
+                                                                              \
+  RSA* rsa = rsa_ ## KEY ## _key(KEY_ARGS_ ## KEY);                           \
+  if (rsa == NULL)                                                            \
+    goto exit;                                                                \
+                                                                              \
+  out_len = RSA_size(rsa);                                                    \
+  out = new char[out_len];                                                    \
+  out_len = RSA_ ## KEY ## _ ## OP(in_len,                                    \
+                                   (unsigned char *) in,                      \
+                                   (unsigned char *) out,                     \
+                                   rsa,                                       \
+                                   pad);                                      \
+  if (out_len > 0)                                                            \
+    fatal = false;                                                            \
+                                                                              \
+ exit:                                                                        \
+  if (rsa != NULL)                                                            \
+    RSA_free(rsa);                                                            \
+                                                                              \
+  if (fatal)                                                                  \
+    return NanThrowError("RSA_" #KEY "_" #OP " failed");                      \
+                                                                              \
+  Local<Value> rc = NanNewBufferHandle(out, out_len);                         \
+  NanReturnValue(rc);                                                         \
+}
 
 using namespace v8;
 using namespace node;
 
-NAN_METHOD(Decrypt) {
-  NanScope();
-  LOG("Beginning Decrypt")
-
-  int out_len = 0;
-  char *out = NULL;
-  int pad = RSA_PKCS1_PADDING;
-  bool fatal = true;
-
-  REQ_BUF_ARG(0, in)
-  REQ_BUF_ARG(1, key_pem)
-
-  RSA* rsa = rsa_private_key(key_pem, key_pem_len, NULL);
-  if (rsa == NULL)
-    goto exit;
-
-  out_len = RSA_size(rsa);
-  out = new char[out_len];
-  out_len = RSA_private_decrypt(in_len,
-                                (unsigned char *) in,
-                                (unsigned char *) out,
-                                rsa,
-                                pad);
-  if (out_len > 0)
-    fatal = false;
-
- exit:
-  if (rsa != NULL)
-    RSA_free(rsa);
-
-  if (fatal)
-    return NanThrowError("RSA_private_decrypt failed");
-
-  Local<Value> rc = NanNewBufferHandle(out, out_len);
-  NanReturnValue(rc);
-}
-
-NAN_METHOD(Encrypt) {
-  NanScope();
-  LOG("Beginning Encrypt")
-
-  int out_len = 0;
-  char *out = NULL;
-  int pad = RSA_PKCS1_PADDING;
-  bool fatal = true;
-
-  REQ_BUF_ARG(0, in)
-  REQ_BUF_ARG(1, key_pem)
-
-  RSA* rsa = rsa_public_key(key_pem, key_pem_len);
-  if (rsa == NULL)
-    goto exit;
-
-  out_len = RSA_size(rsa);
-  out = new char[out_len];
-  out_len = RSA_public_encrypt(in_len,
-                               (unsigned char *) in,
-                               (unsigned char *) out,
-                               rsa,
-                               pad);
-  if (out_len > 0)
-    fatal = false;
-
- exit:
-  if (rsa != NULL)
-    RSA_free(rsa);
-
-  if (fatal)
-    return NanThrowError("RSA_public_encrypt failed");
-
-  Local<Value> rc = NanNewBufferHandle(out, out_len);
-  NanReturnValue(rc);
-}
-
-NAN_METHOD(Sign) {
-  NanScope();
-  LOG("Beginning Sign")
-
-  int out_len = 0;
-  char *out = NULL;
-  int pad = RSA_PKCS1_PADDING;
-  bool fatal = true;
-
-  REQ_BUF_ARG(0, in)
-  REQ_BUF_ARG(1, key_pem)
-
-  RSA* rsa = rsa_private_key(key_pem, key_pem_len, NULL);
-  if (rsa == NULL)
-    goto exit;
-
-  out_len = RSA_size(rsa);
-  out = new char[out_len];
-  out_len = RSA_private_encrypt(in_len,
-                                (unsigned char *) in,
-                                (unsigned char *) out,
-                                rsa,
-                                pad);
-  if (out_len > 0)
-    fatal = false;
-
- exit:
-  if (rsa != NULL)
-    RSA_free(rsa);
-
-  if (fatal)
-    return NanThrowError("RSA_private_encrypt failed");
-
-  Local<Value> rc = NanNewBufferHandle(out, out_len);
-  NanReturnValue(rc);
-}
-
-NAN_METHOD(Verify) {
-  NanScope();
-  LOG("Beginning Verify")
-
-  int out_len = 0;
-  char *out = NULL;
-  int pad = RSA_PKCS1_PADDING;
-  bool fatal = true;
-
-  REQ_BUF_ARG(0, in)
-  REQ_BUF_ARG(1, key_pem)
-
-  RSA* rsa = rsa_public_key(key_pem, key_pem_len);
-  if (rsa == NULL)
-    goto exit;
-
-  out_len = RSA_size(rsa);
-  out = new char[out_len];
-  out_len = RSA_public_decrypt(in_len,
-                               (unsigned char *) in,
-                               (unsigned char *) out,
-                               rsa,
-                               pad);
-  if (out_len > 0)
-    fatal = false;
-
- exit:
-  if (rsa != NULL)
-    RSA_free(rsa);
-
-  if (fatal)
-    return NanThrowError("RSA_public_decrypt failed");
-
-  Local<Value> rc = NanNewBufferHandle(out, out_len);
-  NanReturnValue(rc);
-}
+RSAUTL_METHOD(Decrypt, private, decrypt)
+RSAUTL_METHOD(Encrypt, public,  encrypt)
+RSAUTL_METHOD(Sign,    private, encrypt)
+RSAUTL_METHOD(Verify,  public,  decrypt)
 
 void init(Handle<Object> exports) {
   NODE_SET_METHOD(exports, "decrypt", Decrypt);
